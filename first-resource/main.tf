1498cc440317d5c08c82f0a087ce1619bb1a0216
tomiosikoya <78454977+tomiosikoya@users.noreply.github.com>
2021-05-14 02:09:28 -0400

    Update main.tf

--- first-resource/main.tf
+++ first-resource/main.tf
@@ -1,7 +1,36 @@
 provider "aws" {
-    region = "eu-west-2"
+  region     = "ca-central-1"
+}
+# vpc.tf
+resource "aws_vpc" "vpc23" {
+    cidr_block = "10.10.0.0/20"
+    tags = {
+      Name = "myvpc23"
+      Environment = "dev"
+    }
+  
 }
 
-resource "aws_vpc" "myvpc" {
-    cidr_block = "10.0.0.0/16"
+# ec2.tf
+
+resource "aws_instance" "appserver" {
+    ami           = "ami-08523c5075ba75813"
+    instance_type = "t2.micro"
+    key_name      = "tech23"
+    tags = {
+        Name = "dbserver"
+        Environment = "dev"
+
+    }
+
+}
+
+resource "aws_s3_bucket" "mys3" {
+  bucket = "tomis3"
+  acl    = "private"
+
+  tags = {
+    Name        = "tomi"
+    Environment = "Dev"
+  }
 }
